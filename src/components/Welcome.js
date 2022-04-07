6d553c909e0f7614006d12bf6f0bb458dca00dc3
lihaoyang27 <102766172+lihaoyang27@users.noreply.github.com>
2022-04-07 12:09:34 -0400

    deploy Welcome Page

---
+++ src/components/Welcome.js
@@ -0,0 +1,18 @@
+import '../style/welcome.scss'
+
+
+const Welcome = () => {
+
+    return(
+        <div className='welcomeContainer'>
+
+            <div className='fog'></div>
+            <div className='mask'>
+                <div className='lihaoyang'> </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Welcome
